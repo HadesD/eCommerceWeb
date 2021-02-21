3a32f87349b79510c65ab39a1e8300e706c2e60e
Dark.Hades <dark.hades.1102@gmail.com>
2021-02-21 13:57:18 +0900

    Use new version type

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 @section('title', 'Dashboard | ' . config('app.name'))
 @push('head.css')
-    <link href="{{ asset('css/dashboard/app.css') }}?v=1.0.0" rel="stylesheet">
+    @php
+        $appCssFile = mix('/css/dashboard/app.css');
+    @endphp
+    <link href="{{ $appCssFile }}?v={{ filemtime($appCssFile) }}" rel="stylesheet">
 @endpush
 @section('body.content')
     <div id="app"></div>
 @endsection
 @push('body.js')
-    <script src="{{ mix('js/dashboard/app.js') }}?v=4.1.1"></script>
+    @php
+        $appJsFile = mix('/js/dashboard/app.js');
+    @endphp
+    <script src="{{ $appJsFile }}?v={{ filemtime($appJsFile) }}"></script>
 @endpush
