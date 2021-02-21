cc5c5cc98cadf2bdc8e2da457585c4e3ec366ec6
Dark.Hades <dark.hades.1102@gmail.com>
2021-02-21 20:03:27 +0900

    Locale

--- resources/js/dashboard/MainApp.vue
+++ resources/js/dashboard/MainApp.vue
@@ -5,6 +5,10 @@
 </template>
 <script>
 import locale from 'ant-design-vue/lib/locale-provider/vi_VN';
+import moment from 'moment';
+import 'moment/locale/vi';
+
+moment.locale('vi');
 
 export default {
     data(){
--- resources/js/dashboard/app.js
+++ resources/js/dashboard/app.js
@@ -3,12 +3,12 @@ require('../bootstrap');
 import Vue from 'vue'
 import Antd from 'ant-design-vue';
 import VueLazyload from 'vue-lazyload'
+import VueProgressBar from 'vue-progressbar';
 
 import router from './router.js'
 import MainApp from './MainApp.vue'
 import VueMq from 'vue-mq'
 import UserInfo from './plugins/user-info.js'
-import VueProgressBar from 'vue-progressbar';
 
 Vue.use(Antd);
 Vue.use(VueLazyload);
@@ -16,18 +16,15 @@ Vue.use(VueMq);
 Vue.use(UserInfo);
 
 Vue.use(VueProgressBar, {
-  color: '#9254de',
-  failedColor: '#f5222d',
-  height: '3px',
+    color: '#9254de',
+    failedColor: '#f5222d',
+    height: '3px',
 });
 
 
 const app = new Vue({
     el: '#app',
     router,
-    render (h) {
-        return h(MainApp, {
-        });
-    },
+    render: h => h(MainApp),
 });
 
