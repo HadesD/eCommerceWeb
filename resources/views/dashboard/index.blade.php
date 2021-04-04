0df40164e2b3546ca00ba2582cd57c173341fb7a
Dark.Hades <dark.hades.1102@gmail.com>
2021-04-04 20:09:50 +0900

    CheckUpdate for dashboard@

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 @section('title', 'Dashboard | ' . config('app.name'))
 @push('head.css')
-    <link href="{{ mix('/css/dashboard/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('/css/dashboard/app.css') }}" rel="stylesheet" id="app-css" />
 @endpush
 @section('body.content')
     <div id="app"></div>
 @endsection
 @push('body.js')
-    <script src="{{ mix('/js/dashboard/app.js') }}"></script>
+    <script src="{{ mix('/js/dashboard/app.js') }}" id="app-script"></script>
 @endpush
--- resources/vue/dashboard/layouts/MainApp.vue
+++ resources/vue/dashboard/layouts/MainApp.vue
@@ -13,8 +13,60 @@ moment.locale('vi');
 export default {
     data(){
         return {
-            locale
-        }
+            locale,
+        };
+    },
+
+    mounted() {
+        this.checkUpdate();
+    },
+
+    methods: {
+        getAppVer(fromDoc) {
+            return {
+                css: fromDoc.getElementById('app-css').getAttribute('href').split('?id=')[1],
+                script: fromDoc.getElementById('app-script').getAttribute('src').split('?id=')[1],
+            };
+        },
+        checkUpdate() {
+            let hasNewVer = false;
+
+            axios.get('/dashboard')
+                .then(res => {
+                    const newAppDoc = new DOMParser().parseFromString(res.data, 'text/html');
+
+                    const curVer = this.getAppVer(document);
+                    const newVer = this.getAppVer(newAppDoc);
+
+                    if (curVer.css !== newVer.css || curVer.script !== newVer.script) {
+                        this.$notification.info({
+                            message: 'Có phiên bản mới của website',
+                            description: 'Cần tải lại trang để cập nhật',
+                            duration: 0,
+                            placement: 'bottomLeft',
+                            closeIcon: 'Tải lại',
+                            onClose: function (){
+                                location.reload();
+                            },
+                        });
+
+                        hasNewVer = true;
+                    }
+                })
+                .catch(err => {
+                    if (err.response && err.response.data.message) {
+                        this.$message.error(err.response.data.message);
+                        return;
+                    }
+
+                    this.$message.error(err.message || 'Kiểm tra phiên bản mới thất bại');
+                })
+                .finally(() => {
+                    if (!hasNewVer) {
+                        setTimeout(this.checkUpdate, 60000);
+                    }
+                });
+        },
     },
 }
 </script>
