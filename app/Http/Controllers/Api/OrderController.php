4da3fe26532e1ab4fc4639f31c559b004a87da69
Dark.Hades <dark.hades.1102@gmail.com>
2021-04-12 03:47:09 +0900

    col

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -60,7 +60,7 @@ class OrderController extends Controller
         }
 
         if (isset($request->download)) {
-            $orderQuery = $orderQuery->get()->append(['need_paid_total', 'stock_cost_total', 'stock_earned_total', 'remaining_need_paid_total']);
+            $orderQuery = $orderQuery->get()->append(['stock_cost_total', 'need_paid_total', 'stock_earned_total', 'remaining_need_paid_total']);
 
             switch ($request->download) {
                 case 'csv': {
