c5e6b8ee383efc05e591c22cd31276bc58eefc80
Ichiro <dark.hades.1102@gmail.com>
2021-07-11 02:32:40 +0900

    Update

--- resources/vue/layouts/Main.vue
+++ resources/vue/layouts/Main.vue
@@ -1,8 +1,24 @@
 <template>
-<div>sss</div>
+    <div>{{ msg }}</div>
 </template>
 
 <script>
+import RequestRepository from '../utils/RequestRepository';
+
 export default {
+    data() {
+        return {
+            msg: null,
+        };
+    },
+    mounted() {
+        RequestRepository.get('/sfsdsdf')
+            .then(res => {
+                this.msg = 113;
+            })
+            .catch(error => {
+                this.msg = 111;
+            });
+    },
 }
 </script>
