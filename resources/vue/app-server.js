daad0dbf4c996c32b49fd5fff3f84de33740d0fb
Ichiro <dark.hades.1102@gmail.com>
2021-07-12 07:53:35 +0900

    ud

--- resources/vue/app-server.js
+++ resources/vue/app-server.js
@@ -1,39 +1,3 @@
-// import app from './app';
-// import renderVueComponentToString from 'vue-server-renderer/basic';
-// const renderer = require('vue-server-renderer').createRenderer();
-// const { renderToString } = require('@vue/server-renderer');
-
-// renderToString(app, () => {
-//     dispatch(11);
-//     // dispatch(appContent);
-// });
-
-// renderVueComponentToString(app, (err, res) => {
-//     if (err) throw new Error(err);
-//     dispatch(res);
-// });
-
-// new Promise((resolve, reject) => {
-//     app.$router.push(context.url);
-//     app.$router.onReady(() => {
-//         const matchedComponents = app.$router.getMatchedComponents();
-//         if (!matchedComponents.length) {
-//             return reject({ code: 404 });
-//         }
-//         resolve(app);
-//     }, reject);
-// })
-//     .then(app => {
-//         renderVueComponentToString(app, (err, res) => {
-//             if (err) throw new Error(err);
-//             dispatch(res);
-//         });
-//     })
-//     .catch((err) => {
-//         dispatch(err);
-//     });
-
-//=====
 import { createSSRApp } from 'vue';
 import { renderToString } from '@vue/server-renderer';
 import { createRouter, createMemoryHistory } from 'vue-router';
@@ -52,6 +16,8 @@ app.use(router);
 
 (async () => {
     try {
+        router.push(context.url);
+        await router.isReady();
         dispatch(await renderToString(app));
     } catch (e) {
         dispatch(e.message);
