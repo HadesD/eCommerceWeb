75599fcecd545df6c109356b953027890d657d53
Ichiro <dark.hades.1102@gmail.com>
2021-07-23 02:38:32 +0900

    Load categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -9,8 +9,16 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CategoryController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return new JsonResource(Category::all());
+        $parent_id = Category::query()
+            ->where('slug', $request->parent_slug)
+            ->value('id');
+        return new JsonResource(
+            $parent_id ? Category::query()
+                ->where('parent_id', $parent_id)
+                ->get()
+                : Category::all()
+        );
     }
 }
--- resources/vue/pages/Category.vue
+++ resources/vue/pages/Category.vue
@@ -1,8 +1,42 @@
 <template>
-111
+    <div v-for="cat in categories">{{ cat.name }}</div>
 </template>
 <script>
-export default {
+import {
+    onMounted, ref, watch
+} from 'vue';
+import RequestRepository from '../utils/RequestRepository';
+import { useRoute, } from 'vue-router';
 
+export default {
+    setup() {
+        const route = useRoute();
+
+        const categories = ref([]);
+
+        const loadCategories = (slug) => {
+            RequestRepository.get('/categories', {
+                params: {
+                    parent_slug: slug,
+                },
+            })
+                .then(res => [
+                    categories.value = res.data.data,
+                ]);
+        }
+
+        onMounted(() => {
+            loadCategories(route.params.category_slug);
+        });
+
+        watch(
+            () => route.params.category_slug,
+            newSlug => loadCategories(newSlug)
+        );
+
+        return {
+            categories,
+        };
+    },
 }
 </script>
