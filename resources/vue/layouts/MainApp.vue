1f2b0df74fc6b3313282dd40122ee283966dfc10
Ichiro <dark.hades.1102@gmail.com>
2021-07-26 00:31:41 +0900

    Update

--- resources/vue/layouts/MainApp.vue
+++ resources/vue/layouts/MainApp.vue
@@ -1,24 +1,34 @@
 <template>
-    <a-layout>
-        <Header />
-        <a-layout-content style="margin: 25px 0;">
-            <div :style="{ background: '#fff', padding: '24px', }">
-                <router-view></router-view>
-            </div>
-        </a-layout-content>
-        <Footer />
-    </a-layout>
+    <a-config-provider :locale="locale">
+        <a-layout>
+            <Header />
+            <a-layout-content style="margin: 25px 0;">
+                <div :style="{ background: '#fff', padding: '24px', }">
+                    <router-view></router-view>
+                </div>
+            </a-layout-content>
+            <Footer />
+        </a-layout>
+    </a-config-provider>
 </template>
 
 <script>
 import Header from './Header';
 import Footer from './Footer';
 
+import locale from 'ant-design-vue/lib/locale-provider/vi_VN';
+
 import RequestRepository from '../utils/RequestRepository';
 
 export default {
     components: {
         Header, Footer,
+    },
+
+    setup() {
+        return {
+            locale,
+        };
     },
 }
 </script>
