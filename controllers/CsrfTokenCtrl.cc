9a4a461bbb6ccaf117531132c8435dc7feddb06a
Ichiro <dark.hades.1102@gmail.com>
2022-01-01 05:50:11 +0900

    CSRF Token

---
+++ controllers/CsrfTokenCtrl.cc
@@ -0,0 +1,31 @@
+#include "CsrfTokenCtrl.h"
+
+constexpr auto csrfTokenSessionKey = "csrf_token";
+constexpr auto csrfCookieKey = "XSRF-TOKEN";
+
+void CsrfTokenCtrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    std::string token = "";
+    req->session()->modify<decltype(token)>(
+        csrfTokenSessionKey,
+        [token](auto &curToken)
+        {
+            curToken = token;
+        });
+
+    auto res = HttpResponse::newHttpResponse();
+
+    Cookie tokenCookie(csrfCookieKey, token);
+    tokenCookie.setExpiresDate(trantor::Date::now().after(300));
+    tokenCookie.setHttpOnly(true);
+    tokenCookie.setSecure(true);
+
+    res->addCookie(tokenCookie);
+
+    callback(res);
+}
+
+bool CsrfTokenCtrl::verify(const HttpRequestPtr& req)
+{
+    return req->session()->get<std::string>(csrfTokenSessionKey) == req->getCookie(csrfCookieKey);
+}
