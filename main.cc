ca0df41ad8e8549ef4f13afe8c558f824bcfc813
Ichiro <dark.hades.1102@gmail.com>
2022-01-03 19:32:50 +0900

    Load config

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@ int main() {
     //Set HTTP listener address and port
     // drogon::app().addListener("0.0.0.0",80);
     //Load config file
-    drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("./config.json");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
