8c83f25da7865881803e326bef8112131b8c4f5d
Ichiro <dark.hades.1102@gmail.com>
2022-02-12 22:18:04 +0900

    Update

--- app_helpers/StocksMetaData.hpp
+++ app_helpers/StocksMetaData.hpp
@@ -10,4 +10,64 @@
 namespace app_helpers
 {
     using Stock = drogon_model::web_rinphone::Stocks;
+
+    void stockJsonRow(const drogon::orm::DbClientPtr &dbClient, const Stock& stk, Json::Value &stkRow)
+    {
+        using Category = drogon_model::web_rinphone::Categories;
+        using ProductCategory = drogon_model::web_rinphone::ProductCategories;
+        using Image = drogon_model::web_rinphone::Images;
+        using ProductImage = drogon_model::web_rinphone::ProductImages;
+
+        // categories
+        {
+            auto &stkCatRow = stkRow[Category::tableName];
+            stkCatRow = Json::Value(Json::arrayValue);
+            std::promise<int> pCatRet;
+            auto fCatRet = pCatRet.get_future();
+            stk.getCategory(
+                dbClient,
+                [&stkCatRow, &pCatRet](auto pairRows)
+                {
+                    for (const auto &pairRow : pairRows)
+                    {
+                        stkCatRow.append(pairRow.first.toJson());
+                    }
+
+                    pCatRet.set_value(0);
+                },
+                [&pCatRet](const auto &e)
+                {
+                    LOG_ERROR << e.base().what();
+
+                    pCatRet.set_value(1);
+                });
+            fCatRet.get();
+        }
+
+        // images
+        {
+            auto& stkImgRow = stkRow[Image::tableName];
+            stkImgRow = Json::Value(Json::arrayValue);
+            std::promise<int> pImgRet;
+            auto fImgRet = pImgRet.get_future();
+            stk.getImage(
+                dbClient,
+                [&stkImgRow, &pImgRet](auto pairRows)
+                {
+                    for (const auto &pairRow : pairRows)
+                    {
+                        stkImgRow.append(pairRow.first.toJson());
+                    }
+
+                    pImgRet.set_value(0);
+                },
+                [&pImgRet](const auto &e)
+                {
+                    LOG_ERROR << e.base().what();
+
+                    pImgRet.set_value(1);
+                });
+            fImgRet.get();
+        }
+    }
 }
--- controllers/StockCtrl.cc
+++ controllers/StockCtrl.cc
@@ -15,7 +15,55 @@ using StockImage = drogon_model::web_rinphone::StockImages;
 
 void StockCtrl::get(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
 {
+    auto dbClient = app().getDbClient();
+    orm::Mapper<Stock> stkMap(dbClient);
 
+    Json::Value resJson;
+    auto& resMsg = resJson["message"];
+    HttpStatusCode httpRetCode = HttpStatusCode::k200OK;
+
+    orm::Criteria cnd(Stock::Cols::_deleted_at, orm::CompareOperator::IsNull);
+
+    try
+    {
+        size_t page;
+        size_t limit = 24;
+
+        try
+        {
+            page = std::stoul(req->getParameter("page"));
+            page = (page < 1) ? 1 : page;
+        }
+        catch (...)
+        {
+            page = 1;
+        }
+
+        const auto &stks = stkMap
+                               .orderBy(Stock::Cols::_created_at, orm::SortOrder::DESC)
+                               .paginate(page, limit)
+                               .findBy(cnd);
+        auto &retData = resJson["data"];
+        retData = Json::Value(Json::arrayValue);
+        for (const auto &stk : stks)
+        {
+            app_helpers::stockJsonRow(dbClient, stk, retData.append(stk.toJson()));
+        }
+
+        resJson["total"] = static_cast<uint>(stkMap.count(cnd));
+        resJson["current_page"] = static_cast<uint>(page);
+        resJson["per_page"] = static_cast<uint>(limit);
+    }
+    catch (const std::exception &e)
+    {
+        LOG_ERROR << e.what();
+
+        httpRetCode = HttpStatusCode::k500InternalServerError;
+    }
+
+    auto res = HttpResponse::newHttpJsonResponse(resJson);
+    res->setStatusCode(httpRetCode);
+    callback(res);
 }
 
 void StockCtrl::getOne(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, uint64_t id)
