668f9ed9650636ebe97e6665a32df37acaa62b7b
Ichiro <dark.hades.1102@gmail.com>
2022-02-17 02:08:58 +0900

    Products

--- app_helpers/StocksMetaData.cpp
+++ app_helpers/StocksMetaData.cpp
@@ -18,6 +18,7 @@ namespace app_helpers
         using Image = drogon_model::web_rinphone::Images;
         using Transaction = drogon_model::web_rinphone::Transactions;
         using Order = drogon_model::web_rinphone::Orders;
+        using Product = drogon_model::web_rinphone::Products;
 
         // categories
         {
@@ -120,7 +121,6 @@ namespace app_helpers
             fImgRet.get();
         }
 
-
         // Orders History
         {
             auto& stkOrdersRow = stkRow[Order::tableName];
@@ -149,5 +149,39 @@ namespace app_helpers
             };
             fRet.get();
         }
+
+        // Products
+        {
+            auto& stkProductsRow = stkRow[Product::tableName];
+            stkProductsRow = Json::Value(Json::arrayValue);
+            std::promise<uint8_t> p;
+            auto fRet = p.get_future();
+            *dbClient << "SELECT * FROM products WHERE id IN ("
+                         "SELECT product_id FROM order_products WHERE id IN ("
+                         "SELECT order_product_id FROM order_product_stocks WHERE stock_id = $1"
+                         ")"
+                         ")"
+                      << stk.getValueOfId() >>
+                [&p, &stkProductsRow](const drogon::orm::Result &r)
+            {
+                for (const auto &row : r)
+                {
+                    stkProductsRow.append(Product(row).toJson());
+                }
+
+                p.set_value(0);
+            } >>
+                [&p](const drogon::orm::DrogonDbException &e)
+            {
+                LOG_ERROR << e.base().what();
+                p.set_value(1);
+            };
+            fRet.get();
+        }
+    }
+
+    void stockJsonRowCostTotal(const drogon::orm::DbClientPtr &dbClient,
+                      const drogon_model::web_rinphone::Stocks &stk, Json::Value &stkRow)
+    {
     }
 }
